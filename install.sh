002f70f594485dc48b9210075ec0c807398d67e7
Xianglan Piao <xianglan0502@gmail.com>
2020-04-29 12:39:01 +0900

    [feat] update install.sh - update install.sh setup

--- install.sh
+++ install.sh
@@ -2,40 +2,41 @@
 # File              : install.sh
 # Author            : Xianglan Piao <xianglan0502@gmail.com>
 # Date              : 2013.03.21
-# Last Modified Date: 2018.08.13
+# Last Modified Date: 2020.04.29
 # Last Modified By  : Xianglan Piao <xianglan0502@gmail.com>
 
 
+OS=$(uname -s)
 PACKAGE=("vim" "openssh-server" "tmux" "screen" "exuberant-ctags")
 for p in ${PACKAGE[@]}; do
-    if [[ "$OSTYPE" == "linux-gnu" ]]; then
-        sudo apt-get -y install $p
-        sudo apt-get -y install clang clang-format
-    elif [[ "$OSTYPE" == "darwin17" ]]; then
-        sudo port install $p
-        sudo port install clang
-    else
-        sudo pkg install $p
-    fi
+  if [[ "$OS" == "Linux" ]]; then
+    sudo apt-get -y install $p
+    sudo apt-get -y install clang clang-format
+  elif [[ "$OS" == "Darwin" ]]; then
+    brew install $p
+    brew install clang
+  else
+    pkg install $p
+  fi
 done
 
 
-## clang-format -style=google -dump-config > clang-format-google
-## AccessModifierOffset: -2
+# clang-format -style=google -dump-config > clang-format-google
+# sed -i -e 's/AccessModifierOffset: -1/AccessModifierOffset: -2/' clang-format-google
 cp clang-format-google ~/.clang-format
 
 
 TARGET=("vim" "vimrc" "bashrc" "bash_profile" "gitconfig")
 for t in ${TARGET[@]}; do
-    rm -rf $HOME/.$t
-    ln -s $PWD/$t $HOME/.$t
-    echo $t" is changed"
+  rm -rf $HOME/.$t
+  ln -s $PWD/$t $HOME/.$t
+  echo $t" is changed"
 done
 
 
 if [[ -d "$HOME/.vim/bundle" ]]; then
-    rm -rf $HOME/.vim/bundle
-    mkdir -p $HOME/.vim/bundle
+  rm -rf $HOME/.vim/bundle
+  mkdir -p $HOME/.vim/bundle
 fi
 
 
